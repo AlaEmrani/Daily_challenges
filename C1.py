abe740dfd2b33204db4cc02080a4b93d8eed6636
Apple <apple@Apples-MacBook-Pro-2.local>
2020-07-26 15:09:26 +0430

    adding first day challenge

---
+++ C1.py
@@ -0,0 +1,20 @@
+def num_ways(data):
+    memo = [None]*(len(data)+1)
+    return last_k_ways(data, len(data), memo)
+
+def last_k_ways(data, k, memo):
+    if k == 0:
+        return 1
+    s = len(data)-k
+    if data[s] == 0:
+        return 0
+    if memo[k] is not None:
+        return memo[k]
+    result = last_k_ways(data, k-1, memo)
+    if k>1 and int(data[s:s+2])<27:
+        result += last_k_ways(data, k-2, memo)
+    memo[k] = result
+    print(memo)
+    return result
+
+print(num_ways('1111'))
