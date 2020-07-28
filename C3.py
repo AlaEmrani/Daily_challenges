26fae0a3f14289807c88910fdb3dcc637dffc6a2
Apple <apple@Apples-MacBook-Pro-2.local>
2020-07-28 13:00:15 +0430

    third day challenge compeleted

---
+++ C3.py
@@ -0,0 +1,23 @@
+# third day challenge
+# consider list as a number and add one to it
+
+
+test_list = [9,9,9]
+
+
+def add_one(my_list):
+    if len(my_list) == 1:
+        if my_list[0] <9:
+            my_list[0] = my_list[0] + 1
+            return my_list
+        else:
+            return [1, 0]
+    if my_list[-1] == 9:
+        return add_one(my_list[:-1]) + [0]
+    my_list[-1] = my_list[-1] + 1
+    return my_list
+
+
+print(add_one(test_list))
+
+
